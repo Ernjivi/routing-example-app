1c31e0532562d71bce1dde52d7317c7755ba48fa
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-02-07 16:12:17 -0600

    Imported HttpClientModule on UsersModule

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
@@ -9,7 +10,8 @@ import { UserService } from './services/user.service';
 @NgModule({
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    HttpClientModule,
   ],
   declarations: [
     UserListComponent,
